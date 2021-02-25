ea35c71c838f3b429da23d148c0721b4e2a1fae8
Omarius Credle <ocredle@omariuss-mbp.home>
2021-02-24 23:22:33 -0500

    Created home page and set up routing. Now moving to creating Registration form component.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Route, Switch, Link} from 'react-router-dom';
+import fitPhoto from './Photos/workout-photo.jpg';
+import Registration from './Components/Registration';
+import {useState, useEffect, axios } from 'react';
 
 function App() {
+  const initialForm= {
+    name: '',
+    username:'',
+    email:'',
+    password:'',
+    role:''
+  }
+  const [userForm, setUserForm]= useState(initialForm);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Anywhere Fitness</h1>
+      <h2>Fitness NOW? Fitness Anywhere...</h2>
+      <img src={fitPhoto} alt="Man working out" style={{width:'25%'}}/>
+      <br/>
+      <br/>
+      <div>
+        <Link to='/Login'>
+          <button>Login</button>
+        </Link>
+        &nbsp;&nbsp;
+        <Link to='/Registration'>
+          <button>Registration</button>
+        </Link>
+      </div>
+      <Switch>
+        <Route exact path='/Login'> </Route>
+        <Route exact path='/Registration'> <Registration form={userForm} setForm={setUserForm} /></Route>
+      </Switch>
     </div>
   );
 }
---
+++ front-end/src/Components/Registration.js
@@ -0,0 +1,12 @@
+
+
+export default function Registration(){
+    return(
+        <div>
+            <h1>Registration</h1>
+            <form>
+                <input type="text"/>
+            </form>
+        </div>
+    )
+}
